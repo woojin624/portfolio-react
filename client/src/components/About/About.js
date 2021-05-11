01213150ccc8d1b635fb5cd54d2a92e6ef4f1d58
woojin624 <624jang@gmail.com>
2021-05-12 00:30:51 +0900

    complete styled about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -53,20 +53,22 @@ const About = (props) => {
               <h2>개발자를 위한 개발을 하는 개발자</h2>
               <div className={styles.introDesc}>
                 <div className={styles.line}></div>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga qui nam sit excepturi rerum ullam, nihil ipsam incidunt minus esse itaque eaque tempore facilis quam animi assumenda adipisci, corrupti consectetur?</p>
-                <p>Maiores expedita deserunt at aliquid totam, unde quidem consequuntur dolore? Vitae blanditiis tempore accusamus odio natus dicta repellat ex voluptatum quibusdam laudantium?</p>
-                <p>numquam vero quam pariatur voluptates fuga eum culpa fugit voluptas fugiat!</p>
+                <p>프론트엔드 개발자로서 사용자와 맞닿는 화면을 구성하여 빠르고 정확한 데이터 전달과 흥미로운 인터랙션으로 최고의 사용자 경험을 남기는 것을 우선시 목표로 두고 나아가고 있습니다.</p>
+                <p>또한, 이러한 개발을 할 때 직접 로직을 짜고 화면단을 구성을 하기도 하지만 많은 라이브러리와 오픈소스의 도움을 받으며 혼자 감당하기 힘든 부분을 해결하기도 했습니다. 오픈소스를 뜯어보며 개발자가 개발자를 위해 만든 코드를 보고 저 또한 이러한 것을 만들고 싶다는 생각이 들었습니다.</p>
+                <p>사용자의 경험과 개발자의 경험을 모두 이해하고 있는 사람. 그런 개발자가 제가 바라는 목표입니다.</p>
               </div>
             </article>
             <article className={styles.introRight}>
-              <figure>
-                <img src='/images/profile-img1.png' alt='' />
-              </figure>
-              <div className={styles.contactDetails}>
-                <h2>Contact Details</h2>
-                <p>장우진</p>
-                <p>624jang@gmail.com</p>
-                <p>https://github.com/woojin624</p>
+              <div className={styles.card}>
+                <figure>
+                  <img src='/images/profile-img1.png' alt='' />
+                </figure>
+                <div className={styles.contactDetails}>
+                  <h2>Contact Details</h2>
+                  <p>장우진</p>
+                  <p>624jang@gmail.com</p>
+                  <p>https://github.com/woojin624</p>
+                </div>
               </div>
             </article>
           </section>
