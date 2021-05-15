51607376ddcb7a25764b65d6ce816a6e4cc93fc3
woojin624 <624jang@gmail.com>
2021-05-15 22:28:50 +0900

    changed about framer motion

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -12,6 +12,8 @@ const About = () => {
   console.log('clientWidth', document.documentElement.clientWidth);
   console.log(framer);
 
+  const aboutTitle = ['A', 'B', 'O', 'U', 'T'];
+
   return (
     <motion.div variants={framer.about} initial='hidden' animate='visible' exit='out' className={styles.about}>
       <motion.h3 variants={framer.pageTitle} className={styles.pageTitle}>
@@ -20,16 +22,24 @@ const About = () => {
       <motion.div variants={framer.aboutContain} className={styles.aboutContain}>
         <section className={styles.introSec}>
           <article className={styles.introLeft}>
-            <h1>ABOUT</h1>
-            <h2>개발자를 위한 개발을 하는 개발자</h2>
-            <div className={styles.introDesc}>
+            <h1>
+              {aboutTitle.map((text, i) => {
+                return (
+                  <motion.span variants={framer.mainText} custom={i} key={i}>
+                    {text}
+                  </motion.span>
+                );
+              })}
+            </h1>
+            <motion.h2 variants={framer.ability}>개발자를 위한 개발을 하는 개발자</motion.h2>
+            <motion.div variants={framer.ability} className={styles.introDesc}>
               <div className={styles.line}></div>
               <p>프론트엔드 개발자로서 사용자와 맞닿는 화면을 구성하여 빠르고 정확한 데이터 전달과 흥미로운 인터랙션으로 최고의 사용자 경험을 남기는 것을 우선시 목표로 두고 나아가고 있습니다.</p>
               <p>또한, 이러한 개발을 할 때 직접 로직을 짜고 화면단을 구성을 하기도 하지만 많은 라이브러리와 오픈소스의 도움을 받으며 혼자 감당하기 힘든 부분을 해결하기도 했습니다. 오픈소스를 뜯어보며 개발자가 개발자를 위해 만든 코드를 보고 저 또한 이러한 것을 만들고 싶다는 생각이 들었습니다.</p>
               <p>사용자의 경험과 개발자의 경험을 모두 이해하고 있는 사람. 그런 개발자가 제가 바라는 목표입니다.</p>
-            </div>
+            </motion.div>
           </article>
-          <article className={styles.introRight}>
+          <motion.article variants={framer.ability} className={styles.introRight}>
             <div className={styles.card}>
               <figure>
                 <img src='/images/profile-img1.png' alt='' />
@@ -41,7 +51,7 @@ const About = () => {
                 <p>https://github.com/woojin624</p>
               </div>
             </div>
-          </article>
+          </motion.article>
         </section>
         <motion.section variants={framer.ability} className={styles.abilityLine}>
           <h2>Ability</h2>
--- client/src/framer/about.js
+++ client/src/framer/about.js
@@ -15,19 +15,25 @@ export const about = {
   },
 };
 
-export const pageTitle = {
-  hidden: {
-    x: '-50%',
-    y: '-500%',
-  },
-  visible: {
-    x: '-50%',
-    y: '-50%',
-  },
-  out: {
-    x: '-50%',
-    y: '-500%',
-  },
+export const mainText = {
+  hidden: (i) => ({
+    x: `${i * 1.6}em`,
+    y: 'calc(40vh - 120px)',
+    scale: 3,
+  }),
+  visible: (i) => ({
+    x: `0%`,
+    y: `0%`,
+    scale: 1,
+    transition: {
+      delay: 0.5,
+      duration: 1,
+    },
+  }),
+  out: (i) => ({
+    x: `${-i * 5}%`,
+    y: `${i * 15}%`,
+  }),
 };
 
 export const aboutContain = {
@@ -55,7 +61,7 @@ export const ability = {
     opacity: 1,
     y: 0,
     transition: {
-      delay: 0.2,
+      delay: 5.2,
       duration: 0.4,
     },
   },
@@ -65,5 +71,20 @@ export const ability = {
     transition: {
       duration: 0.4,
     },
+  },
+};
+
+export const pageTitle = {
+  hidden: {
+    x: '-50%',
+    y: '-500%',
+  },
+  visible: {
+    x: '-50%',
+    y: '-50%',
+  },
+  out: {
+    x: '-50%',
+    y: '-500%',
   },
 };
