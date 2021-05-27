91a08ee614708ddd2b1d23ee521195b9a4d84a4c
woojin624 <54699702+woojin624@users.noreply.github.com>
2021-05-27 11:17:03 +0900

    enhanced redux middleware in NODE_ENV production

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middleware = [logger, thunk, promiseMiddleware];
+const enhancer = process.env.NODE_ENV === 'production' ? compose(applyMiddleware([thunk, promiseMiddleware])) : composeWithDevTools(applyMiddleware([logger, thunk, promiseMiddleware]));
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, enhancer);
 
 export default store;
