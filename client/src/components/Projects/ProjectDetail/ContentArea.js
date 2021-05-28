d8341ce08e8c62752187620baa6e8acfd6ba5ae6
woojin624 <54699702+woojin624@users.noreply.github.com>
2021-05-28 19:00:49 +0900

    modified ContentArea

--- client/src/components/Projects/ProjectDetail/ContentArea.js
+++ client/src/components/Projects/ProjectDetail/ContentArea.js
@@ -39,117 +39,131 @@ const ContentArea = ({ project }) => {
   `;
 
   const Content = styled.section`
-    position: relative;
-    width: 100%;
-    min-height: 100px;
-    height: auto;
+    div.tui-editor-contents {
+      position: relative;
+      width: 100%;
+      min-height: 100px;
+      height: auto;
+      font-family: 'Roboto', 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', sans-serif;
 
-    h1 {
-      ${frame}
-      ${title('32px')}
-    }
-
-    h2 {
-      ${frame}
-      ${title('24px')}
-    }
-
-    h3 {
-      ${frame}
-      ${title('18px')}
-    }
-    p {
-      img {
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        display: block;
-      }
-    }
-
-    ul {
-      background: #242424;
-      color: #f8f6f3;
-      padding: 2rem 0;
-      list-style-type: none;
-    }
-
-    ol {
-      background: none;
-      color: #242424;
-      padding: 2rem 0;
-      list-style: none;
-    }
-
-    li {
-      strong {
-        display: block;
-        ${frame}
-        ${title('3rem')}
-        margin-top: 3rem;
+      * {
+        box-sizing: border-box;
       }
 
-      em {
-        display: block;
-        ${frame}
-        ${title('1.5rem')}
-        font-weight: 500;
-      }
-
-      img {
-        ${frame}
-        margin: 0 auto;
-        padding: 1rem 50px;
-        display: block;
-      }
-    }
-
-    blockquote {
-      ${frame};
-      margin: 0 auto;
-      font-size: 1rem;
-      text-align: left;
-      p {
-      }
-    }
-
-    @media all and (max-width: 767px) {
       h1 {
-        ${title('1.3rem')}
+        ${frame}
+        ${title('32px')}
       }
 
       h2 {
-        ${title('18px')}
+        ${frame}
+        ${title('24px')}
       }
 
       h3 {
-        ${title('14px')}
+        ${frame}
+        ${title('18px')}
+      }
+      p {
+        img {
+          width: 100%;
+          margin: 0;
+          padding: 0;
+          display: block;
+        }
       }
 
       ul {
-        padding: 0.5rem 0;
+        background: #242424;
+        color: #f8f6f3;
+        padding: 2rem 0;
+        list-style-type: none;
       }
 
       ol {
-        padding: 0.5rem 0;
+        background: none;
+        color: #242424;
+        padding: 2rem 0;
+        list-style: none;
       }
 
       li {
+        margin: 0 auto;
+        padding: 0;
+
+        &::before {
+          content: none;
+        }
+
         strong {
-          ${title('1.3rem')}
-          margin: 1rem auto;
+          display: block;
+          ${frame}
+          ${title('3rem')}
+          margin-top: 3rem;
         }
 
         em {
-          ${title('0.8rem')}
+          display: block;
+          ${frame}
+          ${title('1.5rem')}
           font-weight: 500;
-          margin: 0.5rem auto;
         }
 
         img {
           ${frame}
-          margin-bottom: 0;
-          padding: 0.5rem 50px;
+          margin: 0 auto;
+          padding: 1rem 50px;
+          display: block;
+        }
+      }
+
+      blockquote {
+        ${frame};
+        margin: 0 auto;
+        font-size: 1rem;
+        text-align: left;
+        p {
+        }
+      }
+
+      @media all and (max-width: 767px) {
+        h1 {
+          ${title('1.3rem')}
+        }
+
+        h2 {
+          ${title('18px')}
+        }
+
+        h3 {
+          ${title('14px')}
+        }
+
+        ul {
+          padding: 0.5rem 0;
+        }
+
+        ol {
+          padding: 0.5rem 0;
+        }
+
+        li {
+          strong {
+            ${title('1.3rem')}
+            margin: 1rem auto;
+          }
+
+          em {
+            ${title('0.8rem')}
+            font-weight: 500;
+            margin: 0.5rem auto;
+          }
+
+          img {
+            ${frame}
+            margin-bottom: 0;
+            padding: 0.5rem 50px;
+          }
         }
       }
     }
