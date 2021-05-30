4945bcdd08c9b410b62bfe21e6077bf9e9f63993
woojin624 <624jang@gmail.com>
2021-05-30 15:19:58 +0900

    contentArea bug fixed

--- client/src/components/Projects/ProjectDetail/ContentArea.js
+++ client/src/components/Projects/ProjectDetail/ContentArea.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import TuiViewer from '../../admin/TuiViewer';
 
+import '../../admin/AdminWriteEditor.scss';
+
 const ContentArea = ({ project }) => {
   const frame = css`
     position: relative;
@@ -31,192 +33,17 @@ const ContentArea = ({ project }) => {
   const ContentTitle = styled.h2`
     ${frame}
     ${title('3rem')}
-    margin: 1rem auto 4rem;
+    margin: 1rem auto;
 
     @media all and (max-width: 767px) {
       ${title('2rem')}
     }
   `;
 
-  const Content = styled.section`
-    div.tui-editor-contents {
-      position: relative;
-      width: 100%;
-      min-height: 100px;
-      height: auto;
-      font-family: 'Roboto', 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', sans-serif;
-
-      * {
-        box-sizing: border-box;
-      }
-
-      h1 {
-        ${frame}
-        ${title('32px')}
-      }
-
-      h2 {
-        ${frame}
-        ${title('24px')}
-      }
-
-      h3 {
-        ${frame}
-        ${title('18px')}
-      }
-      p {
-        img {
-          width: 100%;
-          margin: 0;
-          padding: 0;
-          display: block;
-        }
-      }
-
-      ul {
-        background: #242424;
-        color: #f8f6f3;
-        padding: 2rem 0;
-        list-style-type: none;
-      }
-
-      ol {
-        background: none;
-        color: #242424;
-        padding: 2rem 0;
-        list-style: none;
-      }
-
-      li {
-        margin: 0 auto;
-        padding: 0;
-
-        &::before {
-          content: none;
-        }
-
-        strong {
-          display: block;
-          ${frame}
-          ${title('3rem')}
-          margin-top: 3rem;
-        }
-
-        em {
-          display: block;
-          ${frame}
-          ${title('1.5rem')}
-          font-style: normal;
-          font-weight: 500;
-        }
-
-        img {
-          ${frame}
-          margin: 0 auto;
-          padding: 1rem 50px;
-          display: block;
-        }
-      }
-
-      blockquote {
-        ${frame};
-        margin: 0 auto;
-        font-size: 1rem;
-        text-align: left;
-        p {
-        }
-      }
-
-      @media all and (min-width: 768px) and (max-width: 1023px) {
-        h1 {
-          ${title('1.3rem')}
-        }
-
-        h2 {
-          ${title('18px')}
-        }
-
-        h3 {
-          ${title('14px')}
-        }
-
-        ul {
-          padding: 0.5rem 0;
-        }
-
-        ol {
-          padding: 0.5rem 0;
-        }
-
-        li {
-          strong {
-            ${title('2rem')}
-            margin: 1rem auto;
-          }
-
-          em {
-            ${title('1.2rem')}
-            font-weight: 500;
-            margin: 0.5rem auto;
-          }
-
-          img {
-            ${frame}
-            margin: 0 auto;
-            padding: 1rem 50px;
-          }
-        }
-      }
-
-      @media all and (max-width: 767px) {
-        h1 {
-          ${title('1.3rem')}
-        }
-
-        h2 {
-          ${title('18px')}
-        }
-
-        h3 {
-          ${title('14px')}
-        }
-
-        ul {
-          padding: 0.5rem 0;
-        }
-
-        ol {
-          padding: 0.5rem 0;
-        }
-
-        li {
-          strong {
-            ${title('1.3rem')}
-            margin: 1rem auto;
-          }
-
-          em {
-            ${title('0.8rem')}
-            font-weight: 500;
-            margin: 0.5rem auto;
-          }
-
-          img {
-            ${frame}
-            margin-bottom: 0;
-            padding: 0.5rem 50px;
-          }
-        }
-      }
-    }
-  `;
-
   return (
     <>
       <ContentTitle>Project Detail</ContentTitle>
-      <Content>
-        <TuiViewer content={project.content} />
-      </Content>
+      <TuiViewer content={project.content} />
     </>
   );
 };
